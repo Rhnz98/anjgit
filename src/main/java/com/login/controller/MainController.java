6a8faddf222db1dc55b6a4fc7e6b78f42baa70c1
MAHARJAN ROHIT <c200524@ccg.ac.jp>
2022-05-06 16:51:00 +0900

    third commit

--- src/main/java/com/login/controller/MainController.java
+++ src/main/java/com/login/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	@GetMapping("web")
+	public String show() {
+		return "register";
+	}
 
 }
