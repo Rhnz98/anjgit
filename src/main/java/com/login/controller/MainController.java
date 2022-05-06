99a9182064c57872879f87482e0561074c2c328d
MAHARJAN ROHIT <c200524@ccg.ac.jp>
2022-05-06 16:47:27 +0900

    second commit

--- src/main/java/com/login/controller/MainController.java
+++ src/main/java/com/login/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.login.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MainController {
 
 }
---
+++ src/main/java/com/login/mapper/LoginUserMapper.java
@@ -0,0 +1,5 @@
+package com.login.mapper;
+
+public interface LoginUserMapper {
+
+}
